c0f739af0ab48ea5de6eb49f4511122aa21e8036
Abel Fernández <44572727+abefernan@users.noreply.github.com>
2024-09-18 15:11:13 +0200

    List all spans instead of only root ones

--- components/txs-data.tsx
+++ components/txs-data.tsx
@@ -21,22 +21,20 @@ export default function TxsData() {
     <div>
       <h1>Transactions</h1>
       <ul>
-        {data.txs
-          .filter((tx: Tx) => !tx._source.references.length)
-          .map((tx: Tx) => (
-            <li key={tx._id}>
-              <Link href={`/txs/${tx._source.traceID}`}>
-                {tx._source.traceID} - {tx._source.operationName} -{" "}
-                {
-                  data.txs.filter(
-                    (txToFilter: Tx) =>
-                      txToFilter._source.traceID === tx._source.traceID,
-                  ).length
-                }{" "}
-                spans - {tx._source.tags.length} tags
-              </Link>
-            </li>
-          ))}
+        {data.txs.map((tx: Tx) => (
+          <li key={tx._id}>
+            <Link href={`/txs/${tx._source.traceID}`}>
+              {tx._source.traceID} - {tx._source.operationName} -{" "}
+              {
+                data.txs.filter(
+                  (txToFilter: Tx) =>
+                    txToFilter._source.traceID === tx._source.traceID,
+                ).length
+              }{" "}
+              spans - {tx._source.tags.length} tags
+            </Link>
+          </li>
+        ))}
       </ul>
     </div>
   );

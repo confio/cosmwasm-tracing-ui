e85258de0c34fdd6a2d5e0f71a66ec1813aba83a
Abel Fernández <44572727+abefernan@users.noreply.github.com>
2024-09-30 10:17:41 +0200

    Add tx schema and inferred type

--- types/txs.ts
+++ types/txs.ts
@@ -1,10 +1,36 @@
-export type Tx = {
-  _id: string;
-  _source: {
-    traceID: string;
-    spanID: string;
-    operationName: string;
-    references: Array<{ refType: string; traceID: string; spanID: string }>;
-    tags: Array<{ key: string; type: string; value: string }>;
-  };
-};
+import { z } from "zod";
+
+export const TxSchema = z
+  .object({
+    _id: z.string(),
+    _source: z.object({
+      traceID: z.string(),
+      spanID: z.string(),
+      operationName: z.string(),
+      references: z.array(
+        z.object({
+          refType: z.string(),
+          traceID: z.string(),
+          spanID: z.string(),
+        }),
+      ),
+      tags: z.array(
+        z.object({ key: z.string(), type: z.string(), value: z.string() }),
+      ),
+    }),
+  })
+  .transform((v) => ({
+    //NOTE - Using _id for React keyprop for now since I found different spans with same spanId
+    _id: v._id,
+    traceId: v._source.traceID,
+    spanId: v._source.spanID,
+    operationName: v._source.operationName,
+    parentSpanId:
+      v._source.references.find(
+        (ref) =>
+          ref.traceID === v._source.traceID && ref.refType === "CHILD_OF",
+      )?.spanID ?? null,
+    tags: new Map(v._source.tags.map(({ key, value }) => [key, value])),
+  }));
+
+export type Tx = Readonly<z.infer<typeof TxSchema>>;

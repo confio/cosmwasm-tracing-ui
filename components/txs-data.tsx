041becfc3b5b1062638e9bfa956d1f186feac92a
Abel Fernández <44572727+abefernan@users.noreply.github.com>
2024-10-02 13:08:41 +0200

    Extract react query logic (#12)
    * Add react-query hooks
    
    * Use new hook in txs
    
    * Use new hook in tx detail

--- components/txs-data.tsx
+++ components/txs-data.tsx
@@ -1,26 +1,18 @@
 "use client";
 
-import { Tx, TxSchema } from "@/types/txs";
-import { useQuery } from "@tanstack/react-query";
+import { useTxs } from "@/hooks/api";
+import { Tx } from "@/types/txs";
 import Link from "next/link";
-import { z } from "zod";
 
 export default function TxsData() {
-  const { isPending, error, data } = useQuery({
-    queryKey: ["txs"],
-    queryFn: () =>
-      fetch(
-        `http://localhost:4000/api/v1/txs?operationName=execute_tx&tags=${encodeURIComponent('{"tx":"*Bank(Send*"}')}`,
-      )
-        .then((res) => res.json())
-        .then((json) => json.txs),
-  });
+  const {
+    isPending,
+    error,
+    data: txs,
+  } = useTxs("execute_tx", new Map([["tx", "*Bank(Send*"]]));
 
   if (isPending) return "Loading...";
-
   if (error) return "An error has occurred: " + error.message;
-
-  const txs: Readonly<Array<Tx>> = z.array(TxSchema).parse(data);
 
   return (
     <div>

8156df40069f43074e7286231d7928abcde08e9d
Abel Fernández <44572727+abefernan@users.noreply.github.com>
2024-10-10 11:17:29 +0200

    Add MockApiProvider

--- providers/index.tsx
+++ providers/index.tsx
@@ -1,11 +1,14 @@
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { PropsWithChildren } from "react";
+import MockApiProvider from "./mock-api";
 import ReactQueryProvider from "./react-query";
 
 export default function Providers({ children }: PropsWithChildren) {
   return (
-    <ReactQueryProvider>
-      <TooltipProvider>{children}</TooltipProvider>
-    </ReactQueryProvider>
+    <MockApiProvider>
+      <ReactQueryProvider>
+        <TooltipProvider>{children}</TooltipProvider>
+      </ReactQueryProvider>
+    </MockApiProvider>
   );
 }
